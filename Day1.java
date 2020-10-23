e9ff0373c45bdf004e66b10f46b1532f51e7f040
Prashasti Gupta <princess.prashastigupta@gmail.com>
2020-10-23 09:22:06 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+    public static void main(String args[] ) throws Exception {
+Scanner sc = new Scanner(System.in);
+String line = sc.nextLine();
+System.out.println("Hello Techgig");
+System.out.println(line);
+
+   }
+}
