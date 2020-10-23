b31a9743e1ccc914f558f753f7cb4784585aa2e6
Prashasti Gupta <princess.prashastigupta@gmail.com>
2020-10-23 09:47:16 +0530

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+   public static void main(String args[] ) throws Exception {
+   
+	Scanner sc = new Scanner(System.in);
+   double a = sc.nextDouble();
+   int b = sc.nextInt();
+   int c = sc.nextInt();
+   int si = (int)(a*b*c)/100;
+   System.out.println(si);
+   }
+}
